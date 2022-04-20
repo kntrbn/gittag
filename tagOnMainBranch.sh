b9c9947594b95cc6870f9de1b5f2ee3b1de2f834
kntrbn <ken.tairabune@gmail.com>
2022-04-20 14:57:41 -0700

    Commit by gittag

--- tagOnMainBranch.sh
+++ tagOnMainBranch.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
+TAGNAME=$1
+
 cd /Users/ken/github/gittag
 git fetch origin main
 git add .
 git commit -m "Commit by gittag"
-git tag 0.1.3 main
+git tag $TAGNAME main
 git push gittag main
 git push --tags gittag
