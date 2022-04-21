38facad8687618479c9d131e87fad92444c0ebef
kntrbn <ken.tairabune@gmail.com>
2022-04-21 11:48:12 -0700

    Commit by gittag

--- slashcommand.js
+++ slashcommand.js
@@ -17,16 +17,21 @@ const { exec } = require('child_process');
 // /gittagを送信したときの処理
 app.command('/gittag', async ({ command, ack, say }) => {
   await ack();
-  await say(`Tagging with ${command.text}`);
+  // await say(`Tagging with ${command.text}`);
 
 
 
   exec('/Users/ken/github/gittag/getLatestVersion.sh', (err, stdout, stderr) => {
     // latestVersionTag = ${stdout}
     console.log(`stdout for getversion: ${stdout}`)
+    let latestVersionTag=stdout;
+    console.log(`a variable for getversion: ${latestVersionTag}`)
+
   }
   );
 
+  await say(`Tagging with ${command.text}`);
+  
 
 
   exec('/Users/ken/github/gittag/tagOnMainBranch.sh ' + command.text, (err, stdout, stderr) => {
@@ -40,39 +45,3 @@ app.command('/gittag', async ({ command, ack, say }) => {
   await say(`Successfully Tagged.`);
 });
 
-
-
-
-// const { App } = require('@slack/bolt');
-
-// const app = new App({
-//   token: process.env.SLACK_BOT_TOKEN,
-//   signingSecret: process.env.SLACK_SIGNING_SECRET
-// });
-
-// const { exec } = require('child_process');
-
-
-// (async () => {
-//   // Webアプリの起動
-//   await app.start(3000);
-//   console.log('Bolt app is running!');
-// })();
-
-// // /gittagを送信したときの処理
-// app.command('/gittag', async ({ command, ack, say }) => {
-//   await ack();
-//   await say(`Tagging with ${command.text}`);
-
-
-//       exec('/Users/ken/github/gittag/tagOnMainBranch.sh ' + command.text, (err, stdout, stderr) => {
-//       if (err) {
-//         console.log(`stderr: ${stderr}`)
-//         return
-//       }
-//       console.log(`stdout: ${stdout}`)
-//     }
-//   )
-
-
-// });
