c1cbb54dcff23fda9c2674ac63c8ffe090202e4a
kntrbn <ken.tairabune@gmail.com>
2022-04-22 12:00:21 -0700

    Commit by gittag

--- getLatestVersion.sh
+++ getLatestVersion.sh
@@ -2,6 +2,6 @@
 cd /Users/ken/github/gittag 1>2
 git fetch origin main 1>2
 git checkout main 1>2
-LATEST_VERSION=`git tag | egrep -e "^\d+\.\d+\.\d+$" | sort | tail -1` 1>2
+LATEST_VERSION=`git tag | egrep -e "^\d+\.\d+\.\d+$" | sort -n | tail -1` 1>2
 
 echo -n $LATEST_VERSION
